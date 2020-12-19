5f4080ef5506f2471a456bbe0bddfb7d830ecbca
Lucas Sulzbach <lucas@sulzbach.org>
2020-12-19 19:24:24 -0300

    Restructure

---
+++ model.py
@@ -0,0 +1,83 @@
+import pulp
+
+class Task:
+  '''
+  Tarefa a ser executada
+  '''
+  def __init__(self, runTime):
+    self.runTime = runTime
+    self.vars = []
+
+  def addVar(self, var):
+    '''
+    Associa variável tempo de locação à tarefa
+    '''
+    self.vars.append(var)
+
+
+class Machine:
+  '''
+  Máquina a executar tarefas
+  '''
+  def __init__(self, cost, maxTime):
+    self.cost = cost
+    self.maxTime = maxTime
+
+    self.tasks = []
+    self.vars = []
+
+  def addTask(self, task):
+    '''
+    Associa tarefa a ser executada à máquina
+    '''
+    self.tasks.append(task)
+
+  def addVar(self, var):
+    '''
+    Associa variável tempo de locação à máquina
+    '''
+    self.vars.append(var)
+
+
+class LP:
+  '''
+  Programa Linear
+  '''
+  def __init__(self, tasks, machines):
+    # Problema de minimização:
+    self.problem = pulp.LpProblem(sense=pulp.const.LpMinimize)
+
+    # Para cada máquina no problema:
+    for mdx, machine in enumerate(machines):
+      # Para cada tarefa executável pela máquina:
+      for tdx, task in enumerate(machine.tasks):
+        # Cria variável para o tempo de locação (>= 0):
+        var = pulp.LpVariable("fm{}t{}".format(mdx+1, tdx+1), lowBound=0)
+
+        # Associa variável a suas respectivas máquina e tarefa:
+        machine.addVar(var)
+        task.addVar(var)
+
+    # Para cada máquina no problema:
+    for machine in machines:
+      # Soma dos tempos de locação da máquina <= tempo máximo de uso da máquina:
+      self.problem += sum(machine.vars) <= machine.maxTime
+
+    # Para cada tarefa no problema:
+    for task in tasks:
+      # Soma dos tempos de locação para a tarefa = tempo de execução da tarefa:
+      self.problem += sum(task.vars) == task.runTime
+
+    # Soma dos tempos de locação multiplicados pelos seus custos (objetivo):
+    self.problem += sum([
+      sum([
+        m.cost * var for var in m.vars
+      ]) for m in machines
+    ])
+
+  def solve(self):
+    '''
+    Resolve o programa linear
+    '''
+    self.problem.solve(pulp.PULP_CBC_CMD(msg=False))
+    self.solution = self.problem.objective.value()
--- tarefas.py
+++ tarefas.py
@@ -1,122 +1,31 @@
-#! /usr/bin/env python3
-
 import os
 import sys
-import tempfile
-import subprocess
 
-class Input:
-  def __init__(self):
-    try:
-      self.path = sys.argv[1]
-      self.file = open(self.path, 'r')
-    except IndexError:
-      self.path = None
-      self.file = sys.stdin
+from model import Task, Machine, LP
 
-    self.content = self.file.read()
-    self.lines = self.content.splitlines()
+def parseLine(lines):
+  line = [int(num) for num in lines.pop(0).split(' ')]
 
-  def parseLine(self):
-    line = [int(num) for num in self.lines.pop(0).split(' ')]
+  if len(line) == 0:
+    raise ValueError
+  elif len(line) == 1:
+    return line.pop(0)
+  else:
+    return line
 
-    if len(line) == 0:
-      raise ValueError
-    elif len(line) == 1:
-      return line.pop(0)
-    else:
-      return line
-
-
-class Task:
-  def __init__(self, runTime):
-    self.runTime = runTime
-
-
-class Machine:
-  def __init__(self, cost, maxTime):
-    self.cost = cost
-    self.maxTime = maxTime
-
-    self.tasks = []
-
-  def addTask(self, task):
-    self.tasks.append(task)
-
-
-class LP:
-  def __init__(self, tasks, machines):
-    objFunc = ""
-    for idx in range(1, len(machines)+1):
-      for jdx in range(1, len(tasks)+1):
-        if machines[idx-1].cost:
-          objFunc += " + {}m{}t{}".format(machines[idx-1].cost, idx, jdx)
-    objFunc = "min:" + objFunc[2:] + ";"
-
-    restrictions = []
-
-    for idx in range(1, len(machines)+1):
-      for jdx in range(1, len(tasks)+1):
-        restrictions.append("m{}t{} >= 0;".format(idx, jdx))
-
-    for idx in range(1, len(machines)+1):
-      rest = ""
-      for jdx in range(1, len(tasks)+1):
-        rest += " + m{}t{}".format(idx, jdx)
-      restrictions.append(rest[3:] + " <= {};".format(machines[idx-1].maxTime))
-
-    for jdx in range(1, len(tasks)+1):
-      rest = ""
-      for idx in range(1, len(machines)+1):
-        if tasks[jdx-1] in machines[idx-1].tasks:
-          rest += " + m{}t{}".format(idx, jdx)
-      restrictions.append(rest[3:] + " = {};".format(tasks[jdx-1].runTime))
-
-    self.model = objFunc + "\n\n" + '\n'.join(restrictions)
-
-  def print(self):
-    print(self.model)
-
-  def save(self):
-    fd, path = tempfile.mkstemp()
-    fp = open(path, 'w')
-    fp.write(self.model)
-    fp.close()
-    os.close(fd)
-    return path
-
-
-class Solver:
-  def __init__(self, path, numMachines, numTasks):
-    self.path = path
-    self.numMachines = numMachines
-    self.numTasks = numTasks
-
-  def solve(self):
-    self.sol = subprocess.run(["lp_solve", self.path], stdout=subprocess.PIPE).stdout.decode("utf-8").splitlines()
-
-  def parse(self):
-    out = []
-
-    for idx in range(1, self.numMachines+1):
-      m = []
-      for jdx in range(1, self.numTasks+1):
-        for line in self.sol:
-          if "m{}t{}".format(idx, jdx) in line:
-            m.append(line.split(' ').pop())
-            break
-      out.append(' '.join(m))
-
-    for line in self.sol:
-      if "Value of objective function" in line:
-        out.append(str(int(float(line.split(' ').pop()))))
-        break
-
-    return out
-
+def printLine(line):
+  print(' '.join(["%.1f" % num for num in line]))
 
 if __name__ == "__main__":
-  inp = Input()
+  try:
+    path = sys.argv[1]
+    inp = open(path, 'r')
+  except IndexError:
+    path = None
+    inp = sys.stdin
+
+  content = inp.read()
+  lines = content.splitlines()
 
   # Número de tarefas (n)
   # Número de máquinas (l)
@@ -137,18 +46,25 @@ if __name__ == "__main__":
 
   # Número de tarefas por máquina (s)
   # Índices das tarefas suportadas por máquina (t)
-  for idx in range(numMachines):
+  for mdx in range(numMachines):
     s = inp.parseLine()
     for _ in range(s):
-      machines[idx-1].addTask(tasks[inp.parseLine()-1])
+      machines[mdx-1].addTask(tasks[inp.parseLine()-1])
 
+  # Define problema:
   lp = LP(tasks, machines)
 
-  path = lp.save()
+  # Resolve problema:
+  lp.solve()
 
-  solver = Solver(path, numMachines, numTasks)
-  solver.solve()
+  for machine in machines:
+    line = []
+    for task in tasks:
+      try:
+        tdx = machine.tasks.index(task)
+        line.append(machine.vars[tdx].varValue)
+      except ValueError:
+        line.append(0.0)
+    printLine(line)
 
-  os.remove(path)
-
-  print('\n'.join(solver.parse()))
+  printLine(lp.solution)
